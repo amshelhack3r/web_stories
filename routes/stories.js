03e2cac7d46a35da9e0742e7a3fe6f9ec4e37b25
amshelhack3r <amshelhack3r@gmail.com>
2018-10-27 16:46:40 +0300

    create put and delete routes

--- routes/stories.js
+++ routes/stories.js
@@ -4,16 +4,28 @@ const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 const Story = require("../models/Story");
 
 router.get("/", (req, res) => {
-  Story.find({ status: "public" }).then(stories => {
-    res.render("stories/index", { stories });
-  });
+  Story.find({ status: "public" })
+    .populate("user")
+    .sort({ published_on: "desc" })
+    .then(stories => {
+      res.render("stories/index", { stories });
+    });
+});
+
+router.get("/my", ensureAuthenticated, (req, res) => {
+  Story.find({ user: req.user.id })
+    .populate("user")
+    .sort({ published_on: "desc" })
+    .then(stories => {
+      res.render("stories/index", { stories });
+    });
 });
 /* GET home page. */
 router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("stories/add");
 });
 
-router.post("/add", (req, res) => {
+router.post("/add", ensureAuthenticated, (req, res) => {
   let allowComments;
 
   if (req.body.comment) {
@@ -25,12 +37,75 @@ router.post("/add", (req, res) => {
     title: req.body.title,
     status: req.body.status,
     allowComments,
-    body: req.body.body
+    body: req.body.body,
+    user: req.user.id
   };
 
   new Story(story).save().then(story => {
-    res.redirect("/");
+    res.redirect("/stories");
   });
 });
 
+//show a story
+router.get("/show/:id", (req, res) => {
+  Story.findOne({ _id: req.params.id })
+    .populate("user")
+    .populate("comments.commentUser")
+    .then(story => {
+      res.render("stories/show", { story });
+    });
+});
+
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
+  Story.findById(req.params.id).then(story => {
+    res.render("stories/edit", { story });
+  });
+});
+
+router.put("/edit/:id", ensureAuthenticated, (req, res) => {
+  let allowComments;
+
+  if (req.body.comment) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  story = {
+    title: req.body.title,
+    status: req.body.status,
+    allowComments,
+    body: req.body.body,
+    user: req.user.id
+  };
+
+  Story.findByIdAndUpdate(req.params.id, story).then(() => {
+    res.redirect("/dashboard");
+  });
+});
+router.delete("/delete/:id", ensureAuthenticated, (req, res) => {
+  Story.findByIdAndRemove(req.params.id).then(() => {
+    res.redirect("/dashboard");
+  });
+});
+
+router.post("/comment/:id", ensureAuthenticated, (req, res) => {
+  Story.findById(req.params.id)
+    .then(story => {
+      const newComment = {
+        commentBody: req.body.commentBody,
+        commentUser: req.user.id
+      };
+
+      //add comment to array use unshift to add it to the beginning of an array
+      story.comments.unshift(newComment);
+
+      story.save().then(story => {
+        res.redirect(`/stories/show/${story.id}`);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 module.exports = router;
