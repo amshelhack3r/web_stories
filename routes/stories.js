e6996ee6e380001c1580cbebda64c4d8a89855b6
amshelhack3r <amshelhack3r@gmail.com>
2018-10-27 12:12:54 +0300

    create an add route to add stories

--- routes/stories.js
+++ routes/stories.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
+const Story = require("../models/Story");
 
-/* GET home page. */
 router.get("/", (req, res) => {
-  res.send("stories loaded");
+  Story.find({ status: "public" }).then(stories => {
+    res.render("stories/index", { stories });
+  });
+});
+/* GET home page. */
+router.get("/add", ensureAuthenticated, (req, res) => {
+  res.render("stories/add");
+});
+
+router.post("/add", (req, res) => {
+  let allowComments;
+
+  if (req.body.comment) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+  story = {
+    title: req.body.title,
+    status: req.body.status,
+    allowComments,
+    body: req.body.body
+  };
+
+  new Story(story).save().then(story => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
