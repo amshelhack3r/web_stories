c77edf0c50e27c6fc9abc1ce0fc3260e09d17080
amshelhack3r <amshelhack3r@gmail.com>
2018-10-27 16:46:25 +0300

    create dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Story = require("../models/Story");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
 /* GET home page. */
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+  Story.find({ user: req.user.id }).then(stories => {
+    res.render("index/dashboard", { stories });
+  });
 });
 
 module.exports = router;
