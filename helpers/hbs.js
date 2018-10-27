677bcdbee3c830efc19fca4e64635d9ad59ef443
amshelhack3r <amshelhack3r@gmail.com>
2018-10-27 16:45:07 +0300

    add handlebars helpers

---
+++ helpers/hbs.js
@@ -0,0 +1,32 @@
+const moment = require("moment");
+
+module.exports = {
+  truncate: (str, len) => {
+    if (str.length > len) {
+      const ending = "...";
+      return str.substring(0, len) + ending;
+    } else {
+      return str;
+    }
+  },
+
+  stripTags: input => {
+    return input.replace(/<(?:.|\n)*?>/gm, "");
+  },
+
+  formatDate: (date, format) => {
+    return moment(date).format(format);
+  },
+
+  editIcon: function(storyUser, loggedUser, storyId, floating = true) {
+    if (storyUser == loggedUser) {
+      if (floating) {
+        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fa fa-pencil"></i></a>`;
+      } else {
+        return `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil"></i></a>`;
+      }
+    } else {
+      return "";
+    }
+  }
+};
