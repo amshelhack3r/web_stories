1427b02f0ccd98e6c8f85919d59bf2a741b57428
amshelhack3r <amshelhack3r@gmail.com>
2018-10-27 21:18:21 +0300

    remove unused routes

--- config/passport.js
+++ config/passport.js
@@ -41,69 +41,6 @@ module.exports = passport => {
     )
   );
 
-  passport.use(
-    new FacebookStrategy(
-      {
-        clientID: keys.facebook_auth.clientID,
-        clientSecret: keys.facebook_auth.clientSecret,
-        callbackURL: keys.facebook_auth.callback,
-        profileFields: ["id", "displayName", "photos", "email"]
-      },
-      function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        // console.log(profile.photos[0].value);
-        User.findOne({ oauth_id: profile.id })
-          .then(user => {
-            if (user) {
-              done(null, user);
-            } else {
-              new User({
-                uid: profile.id,
-                name: profile.displayName,
-                avatar: profile.photos[0].value
-              })
-                .save()
-                .then(user => done(null, user));
-            }
-          })
-          .catch(err => console.log(err));
-      }
-    )
-  );
-
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "email"
-      },
-      (email, password, done) => {
-        User.findOne({
-          email
-        })
-          .then(user => {
-            if (!user) {
-              return done(null, false, {
-                message: "Email not found."
-              });
-            }
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-              if (err) throw err;
-              if (isMatch) {
-                return done(null, user);
-              } else {
-                return done(null, false, {
-                  message: "Incorrect password"
-                });
-              }
-            });
-          })
-          .catch(err => {
-            return done(err);
-          });
-      }
-    )
-  );
-
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
